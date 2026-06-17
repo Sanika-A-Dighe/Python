17019d950d463435ab44ad84a28c954866bfa66d
Sanika-A-Dighe <141570480+Sanika-A-Dighe@users.noreply.github.com>
2026-06-17 16:28:25 +0530

    Datatypes

---
+++ datatype.py
@@ -0,0 +1,24 @@
+a = 24 #integer
+b = -4
+print(type(a)) #  type casting is used to check the datatype of the variable 
+print(type(b))
+
+c = 2.4 #float
+d = -6.76353
+print(type(c))#  type casting is used to check the datatype of the variable 
+print(type(d))
+
+e = 34 + 7j #complex (used rarely)
+f = 9 + 5j
+print(type(e))#  type casting is used to check the datatype of the variable 
+print(type(f))
+
+g = "Name" #string 
+h = 'name'
+print(type(g))#  type casting is used to check the datatype of the variable 
+print(type(h))
+
+i = True #Boolean
+j = False 
+print(type(i)) #  type casting is used to check the datatype of the variable 
+print(type(j))
